42150ea7415ec0156a68af6c31b09146851ce876
kasDev18 <87753378+kasDev18@users.noreply.github.com>
2025-03-13 00:21:48 +0800

    Create messages container

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Sidebar from '../../components/sidebar/Sidebar'
+import MessageContainer from '../../components/messages/MessageContainer'
+
+import "../../index.css"
 
 function Home() {
   return (
     <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 p-5'>
       <Sidebar />
+      <MessageContainer/>
     </div>
   )
 }
