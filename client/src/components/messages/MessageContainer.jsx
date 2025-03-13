5054e0193193a7caf76f3fe70d96047cd5a47c91
kasDev18 <87753378+kasDev18@users.noreply.github.com>
2025-03-14 01:45:01 +0800

    Added welcom message to messages container

--- client/src/components/messages/MessageContainer.jsx
+++ client/src/components/messages/MessageContainer.jsx
@@ -1,20 +1,41 @@
 import React from 'react'
 import Messages from './Messages'
 import MessageInput from './MessageInput'
+import {TiMessages} from 'react-icons/ti'
 
-function MessageContainer() {
+
+const  MessageContainer = () => {
+  const [no_chat_selected, setNoChatSelected] = React.useState(false)
+
+  const NoChatSelected = () => {
+    return (
+      <div className='flex items-center justify-center w-full h-full'>
+        <div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
+          <p>Welcome 👋 John Doe</p>
+          <p>Select a chat to start a conversation</p>
+          <TiMessages className='text-3xl md:text-6xl text-center'/>
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div className='md:min-w-[450px] flex flex-col'>
-        <>
-            {/* Header */}
-            <div className='bg-slate-500 px-4 py-2 mb-2'>
-                <span className='label-text'>To: </span><span className='text-gray-900 font-bold'>John Doe</span>
-            </div>
+    <>
+      {no_chat_selected ? 
+        <NoChatSelected/> :
+        <div className='md:min-w-[450px] flex flex-col'>
+          <>
+              {/* Header */}
+              <div className='bg-slate-500 px-4 py-2 mb-2'>
+                  <span className='label-text'>To: </span><span className='text-gray-900 font-bold'>John Doe</span>
+              </div>
 
-            <Messages/>
-            <MessageInput/>
-        </>
-    </div>
+              <Messages/>
+              <MessageInput/>
+          </>
+        </div>
+      }
+    </>
   )
 }
 
