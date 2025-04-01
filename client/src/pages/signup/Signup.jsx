ab28e0e9dd20aa79721fde426ba43b57ac56dbb0
kasDev18 <87753378+kasDev18@users.noreply.github.com>
2025-04-01 15:56:27 +0800

    Create useSignup hooks

--- client/src/pages/signup/Signup.jsx
+++ client/src/pages/signup/Signup.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import GenderCheckbox from "./GenderCheckbox";
 import { Link } from "react-router-dom";
+import useSignup from "../../hooks/useSignup";
 
 const chat_bg = {
   opacity: "10%",
@@ -14,6 +15,26 @@ const chat_bg = {
 };
 
 function Signup() {
+  const [inputs, setInputs] = React.useState({
+    fullName: "",
+    emailAddress: "",
+    password: "",
+    confirmPassword: "",
+    gender: "",
+  });
+  const {loading, signup} = useSignup();
+
+  const handleCheckboxChange = (gender) => {
+    setInputs({
+      ...inputs,
+      gender,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await signup(inputs);
+  };
   return (
     <div className="h-screen w-screen bg-[#984FD2] font-display">
       <div className="background-chat-imgs">
@@ -72,37 +93,56 @@ function Signup() {
               <h1 className="text-3xl font-bold">Get Started Now</h1>
               <h2>Enter your Credentials to Create your account</h2>
             </div>
-            <form action="">
+            <form onSubmit={handleSubmit}>
               <div className="my-10 text-[#EFA21E]">
                 <div className="my-5">
                   <label htmlFor="">Name</label>
                   <input
                     type="text"
                     className="input input-warning bg-transparent"
+                    value={inputs.fullName}
+                    onChange={(e) =>
+                      setInputs({ ...inputs, fullName: e.target.value })
+                    }
                   />
                 </div>
                 <div className="my-5">
                   <label htmlFor="email_address">Email Address</label>
                   <input
-                    type="text"
+                    type="email"
                     className="input input-warning bg-transparent"
+                    value={inputs.emailAddress}
+                    onChange={(e) =>
+                      setInputs({ ...inputs, emailAddress: e.target.value })
+                    }
                   />
                 </div>
                 <div className="my-5">
                   <label htmlFor="password">Password</label>
                   <input
-                    type="text"
+                    type="password"
                     className="input input-warning bg-transparent"
+                    value={inputs.password}
+                    onChange={(e) =>
+                      setInputs({ ...inputs, password: e.target.value })
+                    }
                   />
                 </div>
                 <div className="my-5">
-                  <label htmlFor="">Confirm Password</label>
+                  <label htmlFor="confirm_password">Confirm Password</label>
                   <input
-                    type="text"
+                    type="password"
                     className="input input-warning bg-transparent"
+                    value={inputs.confirmPassword}
+                    onChange={(e) =>
+                      setInputs({ ...inputs, confirmPassword: e.target.value })
+                    }
                   />
                 </div>
-                <GenderCheckbox />
+                <GenderCheckbox
+                  onCheckboxChange={handleCheckboxChange}
+                  selectedGender={inputs.gender}
+                />
                 <div className="flex flex-col justify-center my-5">
                   <button className="btn bg-[#3DA9F1] text-white border-3 border-[#BEE9FF] w-80 my-1">
                     Signup
