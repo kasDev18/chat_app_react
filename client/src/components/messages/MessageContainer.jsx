edfabedc9121b897bd8d59a1945bd3e889ffd3c7
kasDev18 <87753378+kasDev18@users.noreply.github.com>
2025-04-08 20:04:00 +0800

    Create dynamic styles for chat and sidebar

--- client/src/components/messages/MessageContainer.jsx
+++ client/src/components/messages/MessageContainer.jsx
@@ -1,42 +1,51 @@
-import React from 'react'
-import Messages from './Messages'
-import MessageInput from './MessageInput'
-import {TiMessages} from 'react-icons/ti'
+import { useEffect } from "react";
+import Messages from "./Messages";
+import MessageInput from "./MessageInput";
+import { TiMessages } from "react-icons/ti";
+import useConversation from "../../zustand/useConversation";
+import { use } from "react";
 
+const MessageContainer = () => {
+  const { selectedConversation, setSelectedConversation } = useConversation();
 
-const  MessageContainer = () => {
-  const [no_chat_selected, setNoChatSelected] = React.useState(false)
+  useEffect(() => {
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
 
   const NoChatSelected = () => {
     return (
-      <div className='flex items-center justify-center w-full h-full'>
-        <div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
+      <div className="flex items-center justify-center w-full h-full">
+        <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
           <p>Welcome 👋 John Doe</p>
           <p>Select a chat to start a conversation</p>
-          <TiMessages className='text-3xl md:text-6xl text-center'/>
+          <TiMessages className="text-3xl md:text-6xl text-center" />
         </div>
       </div>
-    )
-  }
+    );
+  };
 
   return (
     <>
-      {no_chat_selected ? 
-        <NoChatSelected/> :
-        <div className='md:min-w-[450px] flex flex-col'>
+      {!selectedConversation ? (
+        <NoChatSelected />
+      ) : (
+        <div className="md:min-w-[450px] flex flex-col">
           <>
-              {/* Header */}
-              <div className='bg-slate-500 px-4 py-2 mb-2'>
-                  <span className='label-text'>To: </span><span className='text-gray-900 font-bold'>John Doe</span>
-              </div>
+            {/* Header */}
+            <div className="bg-slate-500 px-4 py-2 mb-2">
+              <span className="label-text">To: </span>
+              <span className="text-gray-900 font-bold">
+                {selectedConversation.fullName}
+              </span>
+            </div>
 
-              <Messages/>
-              <MessageInput/>
+            <Messages />
+            <MessageInput />
           </>
         </div>
-      }
+      )}
     </>
-  )
-}
+  );
+};
 
-export default MessageContainer
+export default MessageContainer;
