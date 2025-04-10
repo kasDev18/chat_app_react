f15070fd4e01a463707f7ef1d440ddd2e2b33d6d
kasDev18 <87753378+kasDev18@users.noreply.github.com>
2025-04-10 21:12:56 +0800

    Revert "Fix: Created search functionality"

--- client/src/components/sidebar/SearchInput.jsx
+++ client/src/components/sidebar/SearchInput.jsx
@@ -1,44 +1,15 @@
-import { useState } from "react";
-import { toast } from "react-hot-toast";
+import React from 'react'
 import { IoSearchSharp } from "react-icons/io5";
-import useConversation from "../../zustand/useConversation";
-import useGetConversations from "../../hooks/useGetConversations";
+
 function SearchInput() {
-  const [search, setSearch] = useState("");
-  const { setSelectedConversation } = useConversation();
-  const { conversations } = useGetConversations();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!search) return;
-    if (search.length < 3)
-      toast.error("Search must be at least 3 characters long");
-
-    const conversation = conversations.find((conversation) => {
-      return conversation.fullName.toLowerCase().includes(search.toLowerCase());
-    });
-
-    if (conversation) {
-      console.log(conversation);
-
-      setSelectedConversation(conversation);
-    } else toast.error("No such user found");
-  };
-
   return (
-    <form className="flex items-center gap-2" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder="Search..."
-        className="input input-border rounded-full"
-      />
-      <button type="submit" className="btn btn-circle bg-sky-500 text-white">
-        <IoSearchSharp className="w-6 h-6 outline-none" />
+    <form className='flex items-center gap-2'>
+      <input type="text" placeholder='Search...' className='input input-border rounded-full' />
+      <button type='submit' className='btn btn-circle bg-sky-500 text-white'>
+        <IoSearchSharp className='w-6 h-6 outline-none'/>
       </button>
     </form>
-  );
+  )
 }
 
-export default SearchInput;
+export default SearchInput
