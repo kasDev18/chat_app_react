a1f4f2e5c7568a4499b9384e8964b18bfdcfee59
kasDev18 <87753378+kasDev18@users.noreply.github.com>
2025-04-13 00:11:40 +0800

    Create real-time users online checker

--- client/src/components/messages/MessageContainer.jsx
+++ client/src/components/messages/MessageContainer.jsx
@@ -3,10 +3,11 @@ import Messages from "./Messages";
 import MessageInput from "./MessageInput";
 import { TiMessages } from "react-icons/ti";
 import useConversation from "../../zustand/useConversation";
-import { use } from "react";
+import {useAuthContext} from "../../context/AuthContext";
 
 const MessageContainer = () => {
   const { selectedConversation, setSelectedConversation } = useConversation();
+  const { authUser } = useAuthContext();
 
   useEffect(() => {
     return () => setSelectedConversation(null);
@@ -16,7 +17,7 @@ const MessageContainer = () => {
     return (
       <div className="flex items-center justify-center w-full h-full">
         <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-          <p>Welcome 👋 John Doe</p>
+          <p>Welcome 👋 {authUser.fullName}</p>
           <p>Select a chat to start a conversation</p>
           <TiMessages className="text-3xl md:text-6xl text-center" />
         </div>
--- client/src/components/sidebar/Conversation.jsx
+++ client/src/components/sidebar/Conversation.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import useConversation from "../../zustand/useConversation";
+import { useSocketContext } from "../../context/SocketContext";
 
 function Conversation({ conversation, lastIdx }) {
   const { selectedConversation, setSelectedConversation } = useConversation();
   const isSelected = selectedConversation?._id === conversation._id;
+
+  const {onlineUsers} = useSocketContext();
+  const isOnline = onlineUsers.includes(conversation._id);
+
 
   return (
     <>
@@ -13,11 +18,11 @@ function Conversation({ conversation, lastIdx }) {
         } hover:bg-sky-500 rounded p-2 py-1 cursor-pointer`}
         onClick={() => setSelectedConversation(conversation)}
       >
-        <div className="avatar online">
-          <div className="w-12 rounded-full">
-            <img src={conversation.profilePic} alt="profile-picture" />
-          </div>
-        </div>
+        <div className={`avatar ${isOnline ? "avatar-online" : ""}`}>
+					<div className='w-12 rounded-full'>
+						<img src={conversation.profilePic} alt='user avatar' />
+					</div>
+				</div>
 
         <div className="flex flex-col flex-1">
           <div className="flex gap-3 justify-between">
