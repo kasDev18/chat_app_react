05b579ea178435d90c1082c6aed69169c301588a
kasDev18 <87753378+kasDev18@users.noreply.github.com>
2025-04-16 00:14:51 +0800

    Create shake animation

--- client/src/components/messages/Message.jsx
+++ client/src/components/messages/Message.jsx
@@ -11,6 +11,7 @@ function Message({message}) {
   const chatClassName = fromMe ? "chat chat-end" : "chat chat-start";
   const profilePic = fromMe ? authUser.profilePic : selectedConversation.profilePic;
   const bubbleBgColor = fromMe ? "bg-blue-500" : "bg-gray-500";
+  const shakeClass = message.shouldShake ? "shake" : "";
 
   return (
     <div className={chatClassName}>
@@ -19,7 +20,7 @@ function Message({message}) {
                 <img alt="Tailwind CSS chat bubble component" src={profilePic} />
             </div>
         </div>
-        <div className={`chat-bubble text-white ${bubbleBgColor} pb-1.5`}>{message.message}</div>
+        <div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-1.5`}>{message.message}</div>
         <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedDateTime}</div>
     </div>
   )
