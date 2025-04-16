d6a4661d9152955623fcd3513611072a574bdf76
kasDev18 <87753378+kasDev18@users.noreply.github.com>
2025-04-16 20:26:54 +0800

    Create sound effect on sending message

--- client/src/hooks/useListenMessages.js
+++ client/src/hooks/useListenMessages.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation";
 
-// import notificationSound from "../assets/sounds/notification.mp3";
+import notificationSound from "../assets/sounds/notification.mp3";
 
 const useListenMessages = () => {
 	const { socket } = useSocketContext();
@@ -12,8 +12,8 @@ const useListenMessages = () => {
 	useEffect(() => {
 		socket?.on("newMessage", (newMessage) => {
 			newMessage.shouldShake = true;
-			// const sound = new Audio(notificationSound);
-			// sound.play();
+			const sound = new Audio(notificationSound);
+			sound.play();
 			setMessages([...messages, newMessage]);
 		});
 
