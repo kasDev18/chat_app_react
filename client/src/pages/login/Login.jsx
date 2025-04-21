911e07f1a8a7e14aa4432d02d9975c31063f1453
kasDev18 <87753378+kasDev18@users.noreply.github.com>
2025-04-21 23:49:01 +0800

    Fix login responsiveness

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -13,23 +13,22 @@ function Login() {
   };
 
   return (
+    // bg-[#984FD2]
     <div className="h-screen w-screen bg-[#984FD2] font-display">
       <div className="triangle absolute"></div>
       <div className="flex flex-1 h-screen w-screen absolute">
         <div className="absolute w-screen flex justify-end p-5">
-          <Link to="/signup" className="btn btn-warning mx-2">
+          <Link to="/signup" className="btn btn-warning btn-sm xl:btn-wide xl:h-[4vh] md:btn-md xl:text-xl mx-2">
             Signup
           </Link>
-          <button className="btn btn-warning btn-circle mx-2">?</button>
+          <button className="btn btn-warning btn-circle mx-2 btn-sm xl:w-[3vw] xl:h-[4vh] xl:text-xl md:btn-md">?</button>
         </div>
-        <div className="w-[50%] flex items-center justify-center">
-          <div className="opacity-8 flex items-center justify-center">
+        <div className="xl:w-[60%] md:w-[90%] w-[200%] flex items-center justify-center border rounded-r-full bg-white">
+          <div className="xl:w-[50%] opacity-8 md:rounded-full flex items-center justify-center bg-white relative xl:top-30 md:top-30">
             <img
-              className=""
+              className="md:w-[90%] w-70"
               src="/images/chat2.png"
-              alt=""
-              width={"100%"}
-              height={"100%"}
+              alt="login-chat-bg"
             />
           </div>
           <form
@@ -38,24 +37,22 @@ function Login() {
           >
             <div className="flex justify-center">
               <img
-                className="chat-bg"
+                className="w-1/2 xl:w-[15vw]"
                 src="/images/chatapp_logo.jpg"
-                alt=""
-                width={"20%"}
-                height={"20%"}
+                alt="login-chatapp-logo"
               />
             </div>
-            <div className="flex flex-col items-center justify-center my-10">
-              <h1 className="font-bold text-[#764C9E] text-xl mb-2">
+            <div className="flex flex-col items-center justify-center my-5">
+              <h1 className="font-bold text-[#764C9E] xl:text-3xl sm:text-sm md:text-xl mb-2">
                 Connect with your favorite people
               </h1>
-              <hr className="bolder border-2 border-[#764C9E] w-90" />
+              <hr className="bolder border-2 border-[#764C9E] xl:w-130 md:w-90 w-70" />
             </div>
-            <div className="w-80 ">
+            <div className="xl:w-120 md:w-80 w-60">
               <div className="">
                 <input
                   type="text"
-                  className="input input-warning text-black w-80 bg-transparent border-none placeholder:text-gray-400"
+                  className="input input-warning xl:w-120 text-black bg-transparent border-none placeholder:text-gray-400 xl:input-xl md:input-lg input-sm"
                   placeholder="Email"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
@@ -65,7 +62,7 @@ function Login() {
               <div className="my-5">
                 <input
                   type="password"
-                  className="input input-warning text-black w-80 bg-transparent border-none placeholder:text-gray-400"
+                  className="input input-warning xl:w-120 text-black bg-transparent border-none placeholder:text-gray-400 xl:input-xl md:input-lg input-sm"
                   placeholder="Password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
@@ -73,15 +70,15 @@ function Login() {
                 <hr className="bolder border-1 border-[#EFA21E]" />
               </div>
               <div className="my-5">
-                <button className="btn btn-outline btn-warning w-80" disabled={loading}>
+                <button className="btn btn-outline btn-warning w-60 xl:w-120 md:w-80 md:btn-lg btn-sm" disabled={loading}>
                   {loading ? <span className="loading loading-spinner"></span> : "Login"}
                 </button>
               </div>
             </div>
           </form>
         </div>
-        <div className="w-[50%] flex items-center justify-center">
-          <img src="/images/cp4.png" alt="" width={"50%"} height={"50%"} />
+        <div className="lg:flex hidden items-center justify-center">
+          <img src="/images/cp4.png" className="w-[100%] h-[50%] xl:w-[100%] xl:h-[80%]" alt="login-mobile-phone"/>
         </div>
       </div>
     </div>
