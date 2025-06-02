0b512c2589cebdd3457d5dd0afb1f4227ea9c8c3
kasDev18 <87753378+kasDev18@users.noreply.github.com>
2025-06-03 01:01:28 +0800

    Get last chat on filter conversation

--- client/src/components/sidebar/Conversations.jsx
+++ client/src/components/sidebar/Conversations.jsx
@@ -8,20 +8,33 @@ function Conversations() {
   const [allReceivers, setAllReceivers] = useState([]);
   const { search, setReceiver } = useConversation();
   const { loading, conversations } = useGetConversations();
-  const { userLastChat, fetching } = useGetLastChat();
-  
+  const { userLastChat, fetching, arrayLastChat } = useGetLastChat();
+
   const getReceiver = async () => {
     conversations.map((conversation) => {
       setAllReceivers((prev) => [...prev, conversation._id]);
-    })
+    });
+  };
 
+  const getIndexLastChat = (conversation) => {
+    let obj;
+    userLastChat.map((user) => {
+      if (
+        user?.senderId === conversation._id ||
+        user?.receiverId === conversation._id
+      )
+        obj = user;
+    });
+
+    const index = arrayLastChat.findIndex((data) => data === obj?.last_chat);
+    return userLastChat[index];
   };
 
   useEffect(() => {
     getReceiver();
     setReceiver(allReceivers);
-  }, [conversations]);
-  
+  }, [conversations, search]);
+
   return (
     <div className="py-2 flex flex-col overflow-auto">
       {search
@@ -34,10 +47,11 @@ function Conversations() {
                 key={conversation._id}
                 conversation={conversation}
                 lastIdx={idx === conversations.length - 1}
+                userLastChat={getIndexLastChat(conversation)}
+                fetching={fetching}
               />
             ))
         : conversations.map((conversation, idx) => (
-
             <Conversation
               key={conversation._id}
               conversation={conversation}
@@ -45,7 +59,7 @@ function Conversations() {
               userLastChat={userLastChat[idx]}
               fetching={fetching}
             />
-            ))}
+          ))}
       {loading ? <span className="loading loading-spinner"></span> : null}
     </div>
   );
--- client/src/hooks/useGetLastChat.js
+++ client/src/hooks/useGetLastChat.js
@@ -11,26 +11,42 @@ import useConversation from "../zustand/useConversation";
  */
 function useGetLastChat() {
   const [fetching, setFetching] = useState(false);
-  const { userLastChat, setUserLastChat, receiver } = useConversation();
+  const { userLastChat, setUserLastChat, receiver, setArrayLastChat, arrayLastChat } = useConversation();
 
   useEffect(() => {
     const getLastChat = async () => {
       setFetching(true);
       try {
         let messages = [];
+        let arrMessages = [];
 
         for(let i = 0; i < receiver.length; i++) {
           const res = await fetch(`/api/messages/${receiver[i]}`);
           const data = await res.json();
 
           if (data.error) throw new Error(data.error); /* Handle Error */
+
+          
           if(data.length === 0){
-            messages.push("");
+            /* If no message, push empty object */
+            messages.push({
+              last_chat: "",
+            });
+            arrMessages.push("");
           }else{
-            messages.push(data[data.length - 1]);
+            /* If message, push last message */
+            messages.push({
+              senderId: data[data.length - 1].senderId,
+              receiverId: data[data.length - 1].receiverId,
+              last_chat: data[data.length - 1].message,
+              createdAt: data[data.length - 1].createdAt,
+              updatedAt: data[data.length - 1].updatedAt
+            });
+            arrMessages.push(data[data.length - 1].message);
           }
         };
-        setUserLastChat(messages);
+        setUserLastChat(messages); /* Set User Last Chat object */
+        setArrayLastChat(arrMessages); /* Set Array Last Chat */
       } catch (error) {
         toast.error(error.message);
       } finally {
@@ -43,7 +59,7 @@ function useGetLastChat() {
     getLastChat();
   }, [receiver, setUserLastChat]);
 
-  return { fetching, userLastChat };
+  return { fetching, userLastChat, arrayLastChat };
 }
 
 export default useGetLastChat;
