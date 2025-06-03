518cac5c32ee5b322c7fdc696e4ea665b4bcbcbd
kasDev18 <87753378+kasDev18@users.noreply.github.com>
2025-06-03 20:14:47 +0800

    Change request domain for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://3.105.54.200:5000',
       }
     }
   }
