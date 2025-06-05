329884af3c9d440a26f090657be1b3587c810566
kasDev18 <87753378+kasDev18@users.noreply.github.com>
2025-06-05 21:51:59 +0800

    update all backend host

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server, io } from "./socket/socket.js";
 // import protectRoute from "./middleware/protectRoute.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 dotenv.config(".env");
 
