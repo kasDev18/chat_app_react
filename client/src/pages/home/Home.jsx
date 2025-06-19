1e6b5ea1eac251291dc83db87cf007814d9be3f7
kasDev18 <87753378+kasDev18@users.noreply.github.com>
2025-06-19 19:33:31 +0800

    create user edit page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import MessageContainer from "../../components/messages/MessageContainer";
+import { useAuthContext } from "../../context/AuthContext";
 
 import "../../index.css";
 
 function Home() {
+  const { authUser } = useAuthContext();
   return (
     <div className="flex h-screen w-screen   md:p-5">
       <Sidebar />
       <MessageContainer />
-      <aside className="lg:grid w-1/4 p-5  hidden bg-gray-800">
+      <aside className="lg:grid w-1/4 p-5 hidden bg-gray-800">
         <div className="h-full flex flex-col items-center">
           <div className="pt-5">
             <img
@@ -20,14 +22,21 @@ function Home() {
             />
           </div>
           <div className="text-center text-amber-500 text-2xl font-bold mt-5">
-            John Doe
+            {authUser.fullName}
           </div>
-          <span className="text-center text-gray-400">
+          <span className="text-center text-gray-500">
             Full Stack Developer
           </span>
-          <div className="flex gap-3 mt-5">
-            <button className="btn btn-active bg-[#984FD2]">Message</button>
-            <button className="btn btn-active bg-[#984FD2]">Edit</button>
+          <div className="flex gap-3 mt-5 w-full">
+            <a href="/user/edit" className="w-full">
+              <button
+                id="user-edit-btn"
+                type="button"
+                className="btn btn-active bg-[#984FD2] w-full"
+              >
+                Edit
+              </button>
+            </a>
           </div>
         </div>
       </aside>
---
+++ client/src/pages/home/UserEdit.jsx
@@ -0,0 +1,68 @@
+import { useAuthContext } from "../../context/AuthContext";
+export default function UserEdit() {
+  const { authUser } = useAuthContext();
+  return (
+    <main className="h-screen w-screen p-5">
+      <h1 className="text-2xl font-bold text-amber-500">Edit Profile</h1>&nbsp;
+      <div className="flex">
+        <section className="flex flex-col gap-3 w-[70%] h-[80vh]">
+          <div className="flex items-center justify-center gap-3 border-2 border-gray-700 pb-3 bg-gray-800 p-5 rounded-2xl">
+            <img
+              className="rounded-full border-2 border-amber-500"
+              width={110}
+              src="https://th.bing.com/th/id/R.e2a8b23a278ed28d089dc4a87dbf50ef?rik=35UwSrV89r9CCw&riu=http%3a%2f%2fgcraftupvc.in%2fassets%2fimages%2fabout%2fabout-shape.png&ehk=xkGFVBAk88y56o6rUD%2fKI%2f0Ke3B28bVGUw8Eebtrjxs%3d&risl=&pid=ImgRaw&r=0"
+              alt=""
+            />
+            <div className="flex flex-col justify-center items-center">
+              <button className="btn btn-active bg-gray-900 btn-md hover:bg-[#984FD2] hover:text-white text-white border-2 hover:border-white border-gray-600">
+                Upload new photo
+              </button>
+              <p className="text-gray-500 text-sm">{authUser.emailAddress}</p>
+            </div>
+          </div>
+          <div className="w-full mx-auto my-5 border-2 border-gray-700 rounded-lg p-5 flex flex-col gap-5 bg-gray-800">
+            <h2 className="text-xl font-bold text-gray-400">Personal Info</h2>
+            <form action="" className="flex flex-col gap-3">
+              <div>
+                <label htmlFor="" className="text-gray-400 text-sm">
+                  Full Name
+                </label>
+                <input type="text" className="input input-bordered w-full cursor-not-allowed text-gray-500 input-sm" value="Kevin Doe" readOnly/>
+              </div>
+              <div>
+                <label htmlFor="" className="text-gray-400 text-sm">
+                  First Name
+                </label>
+                <input type="text" className="input input-bordered w-full input-sm" />
+              </div>
+              <div>
+                <label htmlFor="" className="text-gray-400 text-sm">
+                  Last Name
+                </label>
+                <input type="text" className="input input-bordered w-full input-sm" />
+              </div>
+              <div>
+                <label htmlFor="" className="text-gray-400 text-sm">
+                  Email Address
+                </label>
+                <input type="text" className="input input-bordered w-full input-sm" value={authUser.emailAddress}/>
+              </div>
+              <hr className="border-2 border-gray-700 rounded-2xl"/>
+              <div className="flex justify-end gap-2">
+                <button className="btn btn-active bg-[#984FD2] hover:text-white text-white border-2 hover:border-white border-gray-600 btn-sm" type="submit">Edit Profile</button>
+                <a id="home-back-link" href="/">
+                    <button id="home-back-btn" className="btn btn-active btn-primary btn-sm" type="button">Back</button>
+                </a>
+              </div>
+            </form>
+          </div>
+        </section>
+        <section className="px-5 flex flex-col justify-between gap-3 w-[30%]">
+            <div className="p-5 bg-gray-800 border-2 border-gray-700 rounded-2xl h-[79.2vh]">
+    hey
+            </div>
+        </section>{" "}
+      </div>
+    </main>
+  );
+}
