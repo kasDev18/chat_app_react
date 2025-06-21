87134cc1321a6aa08f8f2a2d8ac08d00cf53efd8
kasDev18 <87753378+kasDev18@users.noreply.github.com>
2025-06-21 19:37:46 +0800

    change user edit structure

---
+++ client/src/components/users/EditAvatar.jsx
@@ -0,0 +1,25 @@
+import { useAuthContext } from "../../context/AuthContext";
+
+export default function EditAvatar() {
+    const { authUser } = useAuthContext();
+  return (
+    <div className="flex items-center justify-center gap-3 border-2 border-gray-700 pb-3 bg-gray-800 p-5 rounded-2xl">
+      <img
+        id="avatar"
+        title="Edit Avatar"
+        loading="lazy"
+        className="rounded-full border-2 border-amber-500"
+        width={110}
+        src="https://th.bing.com/th/id/R.e2a8b23a278ed28d089dc4a87dbf50ef?rik=35UwSrV89r9CCw&riu=http%3a%2f%2fgcraftupvc.in%2fassets%2fimages%2fabout%2fabout-shape.png&ehk=xkGFVBAk88y56o6rUD%2fKI%2f0Ke3B28bVGUw8Eebtrjxs%3d&risl=&pid=ImgRaw&r=0"
+        alt="profile avatar"
+      />
+      <div className="flex flex-col justify-center items-center add-edit-avatar">
+        <input type="file" className="hidden" accept="image/*" id="avatar" name="avatar"/>
+        <button id="edit-avatar-btn" type="button" className="btn btn-active bg-gray-900 btn-md hover:bg-[#984FD2] hover:text-white text-white border-2 hover:border-white border-gray-600">
+          Upload new photo
+        </button>
+        <p className="text-gray-500 text-sm">{authUser.emailAddress}</p>
+      </div>
+    </div>
+  );
+}
--- client/src/pages/home/UserEdit.jsx
+++ client/src/pages/home/UserEdit.jsx
@@ -1,33 +1,25 @@
+import EditAvatar from "../../components/users/EditAvatar";
 import EditForm from "../../components/users/EditForm";
-import { useAuthContext } from "../../context/AuthContext";
 export default function UserEdit() {
-  const { authUser } = useAuthContext();
   return (
     <main className="h-screen w-screen p-5">
       <h1 className="text-2xl font-bold text-amber-500">Edit Profile</h1>&nbsp;
       <div className="flex">
-        <section className="flex flex-col gap-3 w-[70%] h-[80vh]">
-          <div className="flex items-center justify-center gap-3 border-2 border-gray-700 pb-3 bg-gray-800 p-5 rounded-2xl">
-            <img
-              className="rounded-full border-2 border-amber-500"
-              width={110}
-              src="https://th.bing.com/th/id/R.e2a8b23a278ed28d089dc4a87dbf50ef?rik=35UwSrV89r9CCw&riu=http%3a%2f%2fgcraftupvc.in%2fassets%2fimages%2fabout%2fabout-shape.png&ehk=xkGFVBAk88y56o6rUD%2fKI%2f0Ke3B28bVGUw8Eebtrjxs%3d&risl=&pid=ImgRaw&r=0"
-              alt=""
-            />
-            <div className="flex flex-col justify-center items-center">
-              <button className="btn btn-active bg-gray-900 btn-md hover:bg-[#984FD2] hover:text-white text-white border-2 hover:border-white border-gray-600">
-                Upload new photo
-              </button>
-              <p className="text-gray-500 text-sm">{authUser.emailAddress}</p>
-            </div>
-          </div>
-          <EditForm />
-        </section>
-        <section className="px-5 flex flex-col justify-between gap-3 w-[30%]">
+        <div className="flex flex-col gap-3 w-[70%] h-[80vh] user-avatar-section">
+          <section className="user-avatar-section">
+            <EditAvatar />
+            {/* <EditForm /> */}
+          </section>
+          <section className="user-form-section">
+            {/* <EditAvatar /> */}
+            <EditForm />
+          </section>
+        </div>
+        <section className="px-5 flex flex-col justify-between gap-3 w-[30%] user-analytics-section">
           <div className="p-5 bg-gray-800 border-2 border-gray-700 rounded-2xl h-[79.2vh]">
             hey
           </div>
-        </section>{" "}
+        </section>
       </div>
     </main>
   );
