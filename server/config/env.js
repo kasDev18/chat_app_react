93d5513e954a0aafea0b8f6e619fd88b63cc4907
kasDev18 <87753378+kasDev18@users.noreply.github.com>
2025-07-05 02:57:48 +0800

    fix error on cloudinary upload

--- server/config/env.js
+++ server/config/env.js
@@ -1,29 +1,13 @@
-import dotenv from "dotenv";
 
-dotenv.config();
-
-export default function ENV() {
-    const {
-        NODE_ENV,
-        PORT,
-        CLIENT_URL,
-        JWT_SECRET,
-        MONGO_DB_URI, 
-        CLOUDINARY_CLOUD_NAME, 
-        CLOUDINARY_UPLOAD_PRESET, 
-        CLOUDINARY_API_KEY, 
-        CLOUDINARY_API_SECRET, 
-    } = process.env;
-
-    return {
-        NODE_ENV,
-        PORT,
-        CLIENT_URL,
-        JWT_SECRET,
-        MONGO_DB_URI, 
-        CLOUDINARY_CLOUD_NAME, 
-        CLOUDINARY_UPLOAD_PRESET, 
-        CLOUDINARY_API_KEY, 
-        CLOUDINARY_API_SECRET,
-    }
+export default {
+    NODE_ENV: process.env.NODE_ENV,
+    PORT: process.env.PORT,
+    CLIENT_URL: process.env.CLIENT_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
+    MONGO_DB_URI: process.env.MONGO_DB_URI, 
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME, 
+    CLOUDINARY_UPLOAD_PRESET: process.env.CLOUDINARY_UPLOAD_PRESET, 
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY, 
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 }
+
